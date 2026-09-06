09f19707da05ebe1551bcad010d95daf29161f60
Slayniaccc <abdirahman.kha@gmail.com>
2026-09-06 07:59:07 +0100

    Rename package from com.abdirahman to com.slayniaccc

--- backend/src/test/java/com/slayniaccc/sportsconflicttracker/SanityCheckTest.java
+++ backend/src/test/java/com/slayniaccc/sportsconflicttracker/SanityCheckTest.java
@@ -1,4 +1,4 @@
-package com.abdirahman.sportsconflicttracker;
+package com.slayniaccc.sportsconflicttracker;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
